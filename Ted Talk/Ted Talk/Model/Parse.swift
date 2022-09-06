6891856e1e790de79b3e6b27a23ddbce804db071
Angela <angela.lee@globant.com>
2022-09-06 11:00:02 -0300

    corrected unwerap using guard statement instead of ! symbol and created a constant for the url string

--- Ted Talk/Ted Talk/Model/Parse.swift
+++ Ted Talk/Ted Talk/Model/Parse.swift
@@ -16,28 +16,30 @@ public class Parse {
     }
     
     func parseTedTalk(_ tedTalks: String, _ completion: @escaping (Result<[TedTalkData], ParseError>) -> Void ) {
-        DispatchQueue.global(qos: .background).async {
-            let session = URLSession.shared
-            let url = URL(string: "https://gist.githubusercontent.com/gonzaloperretti/0e79c229a5de5bacdd07f402c1a3cefd/raw/975582a4389601caa90d21227446ef2838159176/tedTalks.json")
-            session.dataTask(with: url!) { data, response, error in
-                guard error == nil || data != nil else {
-                    completion(.failure(.clientError))
-                    return
-                }
-                do {
-                    let jsonDecoder = JSONDecoder()
-                    let dataFromJson = try jsonDecoder.decode([TedTalkData].self, from: data!)
-                    completion(.success(dataFromJson))
-                }catch {
-                    completion(.failure(.parseError))
-                }
-                
-                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                    completion(.failure(.serverError))
-                    return
-                }
-            }.resume()
+        let session = URLSession.shared
+        let urlAsString = "https://gist.githubusercontent.com/gonzaloperretti/0e79c229a5de5bacdd07f402c1a3cefd/raw/975582a4389601caa90d21227446ef2838159176/tedTalks.json"
+        let url = URL(string: urlAsString)
+        
+        guard let url = url else {
+            return
         }
+        session.dataTask(with: url) { data, response, error in guard let data = data else {
+            completion(.failure(.fileNotFound))
+            return
+            }
+            do {
+                let jsonDecoder = JSONDecoder()
+                let dataFromJson = try jsonDecoder.decode([TedTalkData].self, from: data)
+                completion(.success(dataFromJson))
+            }catch {
+                completion(.failure(.parseError))
+            }
+                    
+            guard let response = response as? HTTPURLResponse,(200...299).contains(response.statusCode) else{
+                completion(.failure(.serverError))
+                return
+            }
+        }.resume()
     }
 }
 
