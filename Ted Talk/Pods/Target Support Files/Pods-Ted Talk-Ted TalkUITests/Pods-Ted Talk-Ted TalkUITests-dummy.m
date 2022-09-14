301b0c78c59f519051d6ce03d87002a2dcb2ac97
Angela <angela.lee@globant.com>
2022-09-14 10:30:15 -0300

    Deleted pod folder

--- Ted Talk/Pods/Target Support Files/Pods-Ted Talk-Ted TalkUITests/Pods-Ted Talk-Ted TalkUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ted_Talk_Ted_TalkUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Ted_Talk_Ted_TalkUITests
-@end
--- Ted Talk/Pods/Target Support Files/Pods-Ted TalkTests/Pods-Ted TalkTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ted_TalkTests : NSObject
-@end
-@implementation PodsDummy_Pods_Ted_TalkTests
-@end
